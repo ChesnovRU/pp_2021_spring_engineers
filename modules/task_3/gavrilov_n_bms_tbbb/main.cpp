a57117a57b288e1e49d4c0a0fcc5897354c891c1
unknown <pelmeshka1110@gmail.com>
2021-05-28 12:08:11 +0300

    new pull request

--- modules/task_3/gavrilov_n_bms_tbbb/main.cpp
+++ modules/task_3/gavrilov_n_bms_tbbb/main.cpp
@@ -10,7 +10,7 @@
 #include "../../modules/task_3/gavrilov_n_bms_tbbb/BatcherMergingSort.h"
 
 const int dataCount = 1000;
-const int maxSplitCount = 6; // new pull request
+const int maxSplitCount = 6;
 
 bool IsSorted(const std::vector<int>& data) {
     if (data.size() == 0)
