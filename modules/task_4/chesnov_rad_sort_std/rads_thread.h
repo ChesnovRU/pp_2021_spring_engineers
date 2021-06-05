e72313422f5d9bd7b85497c4d540fa79068a5996
ChesnovRU <48468264+ChesnovRU@users.noreply.github.com>
2021-06-05 14:34:58 +0300

    Add files via upload

---
+++ modules/task_4/chesnov_rad_sort_std/rads_thread.h
@@ -0,0 +1,16 @@
+#ifndef MODULES_TASK_4
+#define MODULES_TASK_4
+
+#include <thread>
+#include <vector>
+#include <string>
+using namespace std;
+
+int powervec(vector<int> vect);
+vector<int> randomvec(int size);
+vector<int> rads(vector<int> vect);
+vector<int> rads_thread(vector<int> lisd,int threads);
+vector<int> sum(vector<int> vec1, vector<int> vec2);
+void f(vector<int>& lisd);
+
+#endif
